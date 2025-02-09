a8df4e7f15946832e4f62eaeca86da2bcd27727f
J MacPherson <jtdmacpherson@pinkieboi.com>
2025-02-09 20:52:25 +0000

    add select and save file

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Watermarker
 
 Add watermarks to images using tkinter & pillow
+
+Requires Ubuntu font
+`yay -S ttf-ubuntu-font-family`
--- watermarker.py
+++ watermarker.py
@@ -1,55 +1,57 @@
+import getpass
 from tkinter import *
-from PIL import ImageTk, Image
+import tkinter.filedialog
+from PIL import Image, ImageTk, ImageDraw, ImageFont
+
+
+def display_image(image):
+    new_photo = ImageTk.PhotoImage(image.resize((480, 270)))
+    display_img = canvas.create_image(0, 0, image=new_photo, anchor=NW)
+    canvas.configure(image=display_img)
 
 
 def open_file():
-    file = "/home/pinkieboi/Pictures/bgs/wallpaper1.png"
-    new_file= Image.open(file).resize((480, 270))
-    (img_width, img_height) = new_file.size
-    new_photo = ImageTk.PhotoImage(new_file, size=(img_width, img_height))
-    new_img = canvas.create_image(0, 0, image=new_photo, anchor=NW)
-    canvas.configure(image=new_img)
+    global img
+    global user_photo
+    user_photo = Image.open(tkinter.filedialog.askopenfilename()).resize((1920, 1080)).convert(mode="RGBA")
+    display_image(user_photo)
 
 
 def add_watermark():
-    pass
+    global user_photo
+    global img
+    txt = Image.new("RGBA", (1920, 1080), (255, 255, 255, 0))
+    wmk_fnt = ImageFont.truetype("/usr/share/fonts/ubuntu/Ubuntu-R.ttf", 200)
+    d = ImageDraw.Draw(txt)
+    d.text(xy=(50, 50), text=f"{getpass.getuser()}", fill=(0, 0, 0, 160), font=wmk_fnt)
+    d.text(xy=(500, 400), text=getpass.getuser(), fill=(0, 0, 0, 160), font=wmk_fnt)
+    d.text(xy=(890, 750), text=getpass.getuser(), fill=(0, 0, 0, 160), font=wmk_fnt)
+    user_photo = Image.alpha_composite(user_photo, txt)
+    display_image(user_photo)
 
 
 def save_file():
-    pass
-
-
-def exit_no_save():
-    pass
+    global user_photo
+    user_photo.save(f"{tkinter.filedialog.asksaveasfilename()}.png")
 
 
 FONT = ("Ubuntu", 10, "normal")
 
+user_photo = None
 win = Tk()
 win.title("Watermarker")
 canvas = Canvas(win, width=480)
 no_photo = ImageTk.PhotoImage(image=Image.open("Images/no_image.jpg").resize((480, 270)))
-img= canvas.create_image(0, 0, image=no_photo, anchor=NW)
-
-# Display image to be watermarked
-# new_file = open_file()
-
-# Allow user to add image
-add_img = Button(text="Import Image", font=FONT, highlightthickness=0, justify="center", command=open_file)
-# Button to add watermark
-add_wmk_btn = Button(text="Add Watermark", font=FONT, highlightthickness=0, justify="center", command=add_watermark)
-# Button to save watermarked copy of photo
-save_btn = Button(text="Save As...", font=FONT, highlightthickness=0, justify="center", command=save_file)
-# Button to exit without saving
-exit_btn = Button(text="Exit", font=FONT, highlightthickness=0, justify="center", command=exit_no_save)
-
-# Create input box to name watermarked photo before saving
-
-# Place items on canvas
-add_img.grid(row=0, column=0, columnspan=3)
+img = canvas.create_image(0, 0, image=no_photo, anchor=NW)
 canvas.grid(row=1, column=0, columnspan=3)
+
+add_img = Button(text="Import Image", font=FONT, highlightthickness=0, justify="center", command=open_file)
+add_img.grid(row=0, column=0, columnspan=3)
+add_wmk_btn = Button(text="Add Watermark", font=FONT, highlightthickness=0, justify="center", command=add_watermark)
 add_wmk_btn.grid(row=2, column=0)
+save_btn = Button(text="Save As...", font=FONT, highlightthickness=0, justify="center", command=save_file)
 save_btn.grid(row=2, column=2)
+exit_btn = Button(text="Exit", font=FONT, highlightthickness=0, justify="center", command=exit)
 exit_btn.grid(row=3, column=0, columnspan=3)
 
 win.mainloop()
